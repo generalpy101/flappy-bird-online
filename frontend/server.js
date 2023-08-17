30f87e14f3ff8ce8530d55bef966e683277baf17
Prakash Yogi <prakash.y@fyle.in>
2023-08-18 01:00:39 +0530

    restructuring done in frontend, frontend server running

---
+++ frontend/server.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const morgan = require('morgan')
+const axios = require('axios')
+const dotenv = require('dotenv')
+
+const path = require('path')
+
+dotenv.config()
+
+const HOST = process.env.BASE_URL || 'localhost'
+const PORT = process.env.PORT || 3000
+
+const BASE_PATH = path.join(__dirname)
+const PUBLIC_PATH = path.join(BASE_PATH, 'public')
+const STATIC_PATH = path.join(BASE_PATH, 'public', 'static')
+
+const ENV = process.env.NODE_ENV || 'development'
+
+const app = express()
+
+if (ENV === 'development') {
+  app.use(morgan('dev'))
+} else {
+    app.use(morgan('combined'))
+}
+
+app.use('/static', express.static(STATIC_PATH))
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(PUBLIC_PATH, 'index.html'))
+})
+
+app.listen(PORT, HOST, () => {
+    console.log(`Server running on ${HOST}:${PORT}`)
+})
